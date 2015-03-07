a4483e8757a7718ef603738f07252aa71bf17309
GreenSkinMonster <chartreuse.orc@gmail.com>
2015-03-07 10:15:08 +0800

    show version in about dialog

--- hipda/src/main/java/net/jejer/hipda/ui/AboutDialogPreference.java
+++ hipda/src/main/java/net/jejer/hipda/ui/AboutDialogPreference.java
@@ -1,41 +1,39 @@
 package net.jejer.hipda.ui;
 
-import net.jejer.hipda.R;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.TextView;
+
+import net.jejer.hipda.R;
 
 public class AboutDialogPreference extends DialogPreference {
-	private final String LOG_TAG = getClass().getSimpleName();
 
 	public AboutDialogPreference(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void onClick(DialogInterface dialog, int which) {
-		Log.v(LOG_TAG, "onClick");
-	}
-
-	@Override
-	protected View onCreateDialogView () {
-		Log.v(LOG_TAG, "onCreateDialogView");
-		//return null;
+	protected View onCreateDialogView() {
 		LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(
 				Context.LAYOUT_INFLATER_SERVICE);
 		return inflater.inflate(R.layout.dialog_about, null);
 	}
 
 	@Override
-	protected void onBindDialogView (View view) {
-		Log.v(LOG_TAG, "onBindDialogView");
-		WebView wv = (WebView)view;
+	protected void onBindDialogView(View view) {
+		String versionName = "----";
+		try {
+			versionName = view.getContext().getPackageManager().getPackageInfo(view.getContext().getPackageName(), 0).versionName;
+		} catch (Exception e) {
+		}
+		TextView versionView = (TextView) view.findViewById(R.id.version);
+		versionView.setText(versionName);
+
+		WebView wv = (WebView) view.findViewById(R.id.credits);
 		wv.loadUrl("file:///android_asset/html/about.html");
 	}
 }
