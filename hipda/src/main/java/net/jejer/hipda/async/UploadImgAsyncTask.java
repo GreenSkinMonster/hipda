80164626cc84bd401df10afafde56d142a35f32e
GreenSkinMonster <chartreuse.orc@gmail.com>
2015-03-31 17:16:38 +0800

    clean upload image code

--- hipda/src/main/java/net/jejer/hipda/async/UploadImgAsyncTask.java
+++ hipda/src/main/java/net/jejer/hipda/async/UploadImgAsyncTask.java
@@ -22,7 +22,6 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class UploadImgAsyncTask extends AsyncTask<Bitmap, Integer, Boolean> {
@@ -32,11 +31,10 @@ public class UploadImgAsyncTask extends AsyncTask<Bitmap, Integer, Boolean> {
     private static final int UPLOAD_READ_TIMEOUT = 5 * 60 * 1000;
 
     private UploadImgListener mListener;
-    //private Context mCtx;
     private boolean mResult = true;
     private String mUid;
     private String mHash;
-    private String mPicId = new String();
+    private String mPicId = "";
 
     public UploadImgAsyncTask(Context ctx, UploadImgListener v, String uid, String hash) {
         //mCtx = ctx;
@@ -46,13 +44,14 @@ public class UploadImgAsyncTask extends AsyncTask<Bitmap, Integer, Boolean> {
     }
 
     public interface UploadImgListener {
-        public void updateProgress(int percentage);
+        void updateProgress(int percentage);
 
-        public void complete(boolean result, String id);
+        void complete(boolean result, String id);
     }
 
     @Override
     protected Boolean doInBackground(Bitmap... arg) {
+
         Map<String, String> post_param = new HashMap<String, String>();
 
         post_param.put("uid", mUid);
@@ -92,28 +91,25 @@ public class UploadImgAsyncTask extends AsyncTask<Bitmap, Integer, Boolean> {
     }
 
     private static String getBoundry() {
-        StringBuffer _sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         for (int t = 1; t < 12; t++) {
             long time = System.currentTimeMillis() + t;
             if (time % 3 == 0) {
-                _sb.append((char) time % 9);
+                sb.append((char) time % 9);
             } else if (time % 3 == 1) {
-                _sb.append((char) (65 + time % 26));
+                sb.append((char) (65 + time % 26));
             } else {
-                _sb.append((char) (97 + time % 26));
+                sb.append((char) (97 + time % 26));
             }
         }
-        return _sb.toString();
+        return sb.toString();
     }
 
     private String getBoundaryMessage(String boundary, Map<String, String> params, String fileField, String fileName, String fileType) {
-        StringBuffer res = new StringBuffer("--").append(boundary).append("\r\n");
+        StringBuilder res = new StringBuilder("--").append(boundary).append("\r\n");
 
-        Iterator<String> keys = params.keySet().iterator();
-
-        while (keys.hasNext()) {
-            String key = (String) keys.next();
-            String value = (String) params.get(key);
+        for (String key : params.keySet()) {
+            String value = params.get(key);
             res.append("Content-Disposition: form-data; name=\"")
                     .append(key).append("\"\r\n").append("\r\n")
                     .append(value).append("\r\n").append("--")
@@ -137,19 +133,138 @@ public class UploadImgAsyncTask extends AsyncTask<Bitmap, Integer, Boolean> {
             return false;
         }
 
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-        Log.v(LOG_TAG, "Img Original：" + baos.toByteArray().length + "byte");
-        if (baos.toByteArray().length / 1024 > 1024) {//Check and avoid BitmapFactory.decodeStream FC
-            baos.reset();
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 50, baos);
+
+        ByteArrayOutputStream baos = compressImage(bitmap);
+        if (baos == null) {
+            return false;
         }
+
+        // update progress for start upload
+        publishProgress(0);
+
+        byte[] barry = null;
+        int contentLength = 0;
+        String sendStr = "";
+        try {
+            barry = ("--" + BOUNDARYSTR + "--\r\n").getBytes("UTF-8");
+            sendStr = getBoundaryMessage(BOUNDARYSTR, param, imageParamName, "HiPDA_UPLOAD.jpg", fileType);
+            contentLength = sendStr.getBytes("UTF-8").length + baos.size() + 2 * barry.length;
+        } catch (UnsupportedEncodingException ignored) {
+
+        }
+        String lenstr = Integer.toString(contentLength);
+
+        HttpURLConnection urlConnection = null;
+        BufferedOutputStream out = null;
+        try {
+            URL url = new URL(urlStr);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            urlConnection.setRequestProperty("User-Agent", HiUtils.UserAgent);
+            urlConnection.setRequestProperty("Cookie", "cdb_auth=" + HiSettingsHelper.getInstance().getCookieAuth());
+
+            urlConnection.setConnectTimeout(UPLOAD_CONNECT_TIMEOUT);
+            urlConnection.setReadTimeout(UPLOAD_READ_TIMEOUT);
+            urlConnection.setDoInput(true);
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setUseCaches(false);
+            urlConnection.setRequestProperty("Connection", "Keep-Alive");
+            urlConnection.setRequestProperty("Content-type", "multipart/form-data;boundary=" + BOUNDARYSTR);
+            urlConnection.setRequestProperty("Content-Length", lenstr);
+            urlConnection.setFixedLengthStreamingMode(contentLength);
+            urlConnection.connect();
+
+            out = new BufferedOutputStream(urlConnection.getOutputStream());
+            out.write(sendStr.getBytes("UTF-8"));
+
+            int bytesLeft;
+            int transferred = 0;
+            int postSize;
+            int maxPostSize = 1024;
+
+            bytesLeft = baos.size();
+            postSize = Math.min(bytesLeft, maxPostSize);
+            final Thread thread = Thread.currentThread();
+            while (bytesLeft > 0) {
+
+                if (thread.isInterrupted()) {
+                    throw new InterruptedIOException();
+                }
+                out.write(baos.toByteArray(), transferred, postSize);
+                transferred += postSize;
+                bytesLeft -= postSize;
+                postSize = Math.min(bytesLeft, maxPostSize);
+                if (transferred % 50 == 0)
+                    out.flush();
+                publishProgress((int) ((transferred * 100) / baos.size()));
+            }
+
+            //yes, write twice
+            out.write(barry);
+            out.write(barry);
+            out.flush();
+            int status = urlConnection.getResponseCode();
+
+            if (status != HttpURLConnection.HTTP_OK) {
+                mResult = false;
+            }
+            Log.v(LOG_TAG, "uploading image, response : " + urlConnection.getResponseCode() + ", " + urlConnection.getResponseMessage());
+            InputStream in = urlConnection.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String inputLine = "";
+            while ((inputLine = br.readLine()) != null) {
+                mPicId += inputLine;
+            }
+
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error uploading image : " + e.getMessage());
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException ignored) {
+
+                }
+            }
+            try {
+                baos.close();
+            } catch (IOException ignored) {
+
+            }
+            if (urlConnection != null)
+                urlConnection.disconnect();
+        }
+
+        return true;
+    }
+
+    private ByteArrayOutputStream compressImage(Bitmap bitmap) {
+
+        int maxQuality = 90;
+        int maxImageSize = 300 * 1024; // max file size 300K
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(CompressFormat.JPEG, maxQuality, baos);
+
+        int origionalSize = baos.size();
+        if (origionalSize < maxImageSize * 0.8f)
+            return baos;
+
+        //Check and avoid BitmapFactory.decodeStream FC
+        if (baos.size() / 1024 > 1024) {
+            baos.reset();
+            bitmap.compress(CompressFormat.JPEG, 50, baos);
+        }
+
+        //ignore ide suggestion, and do not change following 5 lines, unless you know exactly what to do
         ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());
         BitmapFactory.Options newOpts = new BitmapFactory.Options();
-
         newOpts.inJustDecodeBounds = true;
         Bitmap newbitmap = BitmapFactory.decodeStream(isBm, null, newOpts);
         newOpts.inJustDecodeBounds = false;
+
         int w = newOpts.outWidth;
         int h = newOpts.outHeight;
 
@@ -170,118 +285,15 @@ public class UploadImgAsyncTask extends AsyncTask<Bitmap, Integer, Boolean> {
         newbitmap = BitmapFactory.decodeStream(isBm, null, newOpts);
 
         // HiPDA have 300KB limitation
-        int quality = 100;
+        int quality = maxQuality;
         baos.reset();
         newbitmap.compress(CompressFormat.JPEG, quality, baos);
-        while (baos.toByteArray().length / 1024 > 300) {
+        while (baos.toByteArray().length > maxImageSize) {
             quality -= 10;
             baos.reset();
             newbitmap.compress(CompressFormat.JPEG, quality, baos);
         }
-        byte[] imgBytes = baos.toByteArray();
-        Log.v(LOG_TAG, "Img Compressed: " + quality + "% size："
-                + imgBytes.length + "byte");
-
-
-        // update progress for start upload
-        publishProgress(0);
-
-        byte[] barry = null;
-        int contentLength = 0;
-        String sendStr = "";
-        try {
-            barry = ("--" + BOUNDARYSTR + "--\r\n").getBytes("UTF-8");
-
-            sendStr = getBoundaryMessage(BOUNDARYSTR, param, imageParamName, "HiPDA_UPLOAD.jpg", fileType);
-            contentLength = sendStr.getBytes("UTF-8").length + (int) imgBytes.length + 2 * barry.length;
-        } catch (UnsupportedEncodingException e) {
-
-        }
-        String lenstr = Integer.toString(contentLength);
-
-        HttpURLConnection urlConnection = null;
-        BufferedOutputStream out = null;
-        //FileInputStream fis = null;
-        try {
-            URL url = new URL(urlStr);
-
-            urlConnection = (HttpURLConnection) url.openConnection();
-
-            urlConnection.setRequestProperty("User-Agent", HiUtils.UserAgent);
-            urlConnection.setRequestProperty("Cookie", "cdb_auth=" + HiSettingsHelper.getInstance().getCookieAuth());
-
-            urlConnection.setConnectTimeout(UPLOAD_CONNECT_TIMEOUT);
-            urlConnection.setReadTimeout(UPLOAD_READ_TIMEOUT);
-            urlConnection.setDoInput(true);
-            urlConnection.setDoOutput(true);
-            urlConnection.setRequestMethod("POST");
-            urlConnection.setUseCaches(false);
-            urlConnection.setRequestProperty("Connection", "Keep-Alive");
-            urlConnection.setRequestProperty("Content-type", "multipart/form-data;boundary=" + BOUNDARYSTR);
-            urlConnection.setRequestProperty("Content-Length", lenstr);
-            ((HttpURLConnection) urlConnection).setFixedLengthStreamingMode(contentLength);
-            urlConnection.connect();
-
-            out = new BufferedOutputStream(urlConnection.getOutputStream());
-            out.write(sendStr.getBytes("UTF-8"));
-
-            //fis = new FileInputStream(targetFile);
-
-            int bytesLeft;
-            int transferred = 0;
-            int postSize;
-            int maxPostSize = 1 * 1024;
-
-            bytesLeft = imgBytes.length;
-            postSize = Math.min(bytesLeft, maxPostSize);
-            final Thread thread = Thread.currentThread();
-            while (bytesLeft > 0) {
-
-                if (thread.isInterrupted()) {
-                    throw new InterruptedIOException();
-                }
-                out.write(imgBytes, transferred, postSize);
-                transferred += postSize;
-                bytesLeft -= postSize;
-                postSize = Math.min(bytesLeft, maxPostSize);
-                if (transferred % 50 == 0)
-                    out.flush();
-                publishProgress((int) ((transferred * 100) / imgBytes.length));
-            }
-
-
-            out.write(barry);
-            out.write(barry);
-            out.flush();
-            out.close();
-            int status = urlConnection.getResponseCode();
-
-            if (status != HttpURLConnection.HTTP_OK) {
-                mResult = false;
-            }
-
-            InputStream in = urlConnection.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            String inputLine = "";
-            while ((inputLine = br.readLine()) != null) {
-                mPicId += inputLine;
-            }
-            Log.v("PIC_ID", mPicId);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (out != null) {
-                try {
-                    out.close();
-                } catch (IOException ignored) {
-
-                }
-            }
-            if (urlConnection != null)
-                urlConnection.disconnect();
-        }
-
-        return true;
+        Log.v(LOG_TAG, "Img Compressed: " + quality + "%,  size: " + baos.size() + " bytes, original size: " + origionalSize + " bytes");
+        return baos;
     }
 }
