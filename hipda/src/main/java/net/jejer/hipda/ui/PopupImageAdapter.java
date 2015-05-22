32fb3be8880c3773dcb82911a44bfcaaffb7131b
GreenSkinMonster <chartreuse.orc@gmail.com>
2015-05-23 07:13:33 +0800

    clean image loading progress bar

--- hipda/src/main/java/net/jejer/hipda/ui/PopupImageAdapter.java
+++ hipda/src/main/java/net/jejer/hipda/ui/PopupImageAdapter.java
@@ -6,8 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
+import android.widget.ProgressBar;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.Priority;
@@ -78,19 +77,19 @@ public class PopupImageAdapter extends PagerAdapter {
     private void displayImage(View rootView, ImageReadyInfo imageReadyInfo) {
 
         final SubsamplingScaleImageView wvImage = (SubsamplingScaleImageView) rootView.findViewById(R.id.wv_image);
-        final RelativeLayout loadingPanel = (RelativeLayout) rootView.findViewById(R.id.loadingPanel);
+        final ProgressBar progressBar = (ProgressBar) rootView.findViewById(R.id.loadingPanel);
         final ImageView gifImageView = (ImageView) rootView.findViewById(R.id.gif_image);
 
         if (imageReadyInfo == null) {
             gifImageView.setVisibility(View.VISIBLE);
             wvImage.setVisibility(View.GONE);
-            loadingPanel.setVisibility(View.GONE);
+            progressBar.setVisibility(View.GONE);
             gifImageView.setImageDrawable(mCtx.getResources().getDrawable(R.drawable.tapatalk_image_broken));
         } else {
             if (imageReadyInfo.isGif()) {
                 gifImageView.setVisibility(View.VISIBLE);
                 wvImage.setVisibility(View.GONE);
-                loadingPanel.setVisibility(View.GONE);
+                progressBar.setVisibility(View.GONE);
                 Glide.with(mCtx)
                         .load(imageReadyInfo.getPath())
                         .asGif()
@@ -101,7 +100,9 @@ public class PopupImageAdapter extends PagerAdapter {
                         .into(gifImageView);
             } else {
                 gifImageView.setVisibility(View.GONE);
-                wvImage.setBackgroundColor(mCtx.getResources().getColor(R.color.night_background));
+                wvImage.setVisibility(View.VISIBLE);
+
+//                wvImage.setBackgroundColor(mCtx.getResources().getColor(R.color.night_background));
                 wvImage.setMinimumDpi(100);
                 wvImage.setMinimumTileDpi(160);
                 wvImage.setOrientation(SubsamplingScaleImageView.ORIENTATION_USE_EXIF);
@@ -110,15 +111,14 @@ public class PopupImageAdapter extends PagerAdapter {
                 wvImage.setOnImageEventListener(new SubsamplingScaleImageView.DefaultOnImageEventListener() {
                     @Override
                     public void onImageLoaded() {
-                        loadingPanel.setVisibility(View.GONE);
-                        wvImage.setVisibility(View.VISIBLE);
+                        progressBar.setVisibility(View.GONE);
                     }
 
                     @Override
                     public void onImageLoadError(Exception e) {
-                        loadingPanel.setVisibility(View.GONE);
+                        progressBar.setVisibility(View.GONE);
                         wvImage.setImage(ImageSource.resource(R.drawable.tapatalk_image_broken));
-                        Toast.makeText(mCtx, "图片加载失败", Toast.LENGTH_LONG).show();
+//                        Toast.makeText(mCtx, "图片加载失败", Toast.LENGTH_LONG).show();
                         Logger.e("loading error", e);
                     }
                 });
