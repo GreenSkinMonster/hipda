313fc7feb5a8d44994384a4916c472880b4b0ff4
GreenSkinMonster <chartreuse.orc@gmail.com>
2015-09-10 15:40:44 +0800

    improve fragment transaction?

--- hipda/src/main/java/net/jejer/hipda/ui/BaseFragment.java
+++ hipda/src/main/java/net/jejer/hipda/ui/BaseFragment.java
@@ -1,11 +1,15 @@
 package net.jejer.hipda.ui;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
 import android.app.Activity;
 import android.app.Fragment;
 import android.support.annotation.StringRes;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import com.bumptech.glide.Glide;
 import com.mikepenz.materialdrawer.Drawer;
 
 import net.jejer.hipda.utils.Logger;
@@ -82,5 +86,43 @@ public abstract class BaseFragment extends Fragment {
     public void onDestroyView() {
         stopScroll();
         super.onDestroyView();
+    }
+
+    @Override
+    public Animator onCreateAnimator(int transit, boolean enter, int nextAnim) {
+        //http://daniel-codes.blogspot.sg/2013/09/smoothing-performance-on-fragment.html
+        Animator animator = super.onCreateAnimator(transit, enter, nextAnim);
+        if (animator == null && nextAnim != 0) {
+            animator = AnimatorInflater.loadAnimator(getActivity(), nextAnim);
+            if (animator != null) {
+                if (getView() != null)
+                    getView().setLayerType(View.LAYER_TYPE_HARDWARE, null);
+                animator.addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        Glide.with(getActivity()).pauseRequests();
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        if (Glide.with(getActivity()).isPaused())
+                            Glide.with(getActivity()).resumeRequests();
+                        if (getView() != null)
+                            getView().setLayerType(View.LAYER_TYPE_NONE, null);
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        if (Glide.with(getActivity()).isPaused())
+                            Glide.with(getActivity()).resumeRequests();
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+                    }
+                });
+            }
+        }
+        return animator;
     }
 }
