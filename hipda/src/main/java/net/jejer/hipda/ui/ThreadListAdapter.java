12b213b42cbc3d217f2236835e8bf876915bc63e
GreenSkinMonster <chartreuse.orc@gmail.com>
2015-10-16 14:18:03 +0800

    better thread type label

--- hipda/src/main/java/net/jejer/hipda/ui/ThreadListAdapter.java
+++ hipda/src/main/java/net/jejer/hipda/ui/ThreadListAdapter.java
@@ -1,7 +1,6 @@
 package net.jejer.hipda.ui;
 
 import android.content.Context;
-import android.text.Html;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -40,31 +39,28 @@ public class ThreadListAdapter extends HiAdapter<ThreadBean> {
 
         holder.avatar = (ImageView) convertView.findViewById(R.id.iv_avatar);
         holder.tv_author = (TextView) convertView.findViewById(R.id.tv_author);
+        holder.tv_thread_type = (TextView) convertView.findViewById(R.id.tv_thread_type);
         holder.tv_title = (TextView) convertView.findViewById(R.id.tv_title);
-//        holder.tv_viewcounter = (TextView) convertView.findViewById(R.id.tv_viewcounter);
         holder.tv_replycounter = (TextView) convertView.findViewById(R.id.tv_replycounter);
         holder.tv_create_time = (TextView) convertView.findViewById(R.id.tv_create_time);
-//        holder.tv_update_time = (TextView) convertView.findViewById(R.id.tv_update_time);
         holder.iv_image_indicator = (TextView) convertView.findViewById(R.id.iv_image_indicator);
 
         holder.tv_author.setText(thread.getAuthor());
+        holder.tv_title.setText(thread.getTitle());
 
         if (HiSettingsHelper.getInstance().isShowPostType() &&
                 !TextUtils.isEmpty(thread.getType())) {
-            holder.tv_title.setText(Html.fromHtml("<font color=grey>[" + thread.getType() + "]</font>" + thread.getTitle()));
+            holder.tv_thread_type.setText(thread.getType());
+            holder.tv_thread_type.setVisibility(View.VISIBLE);
         } else {
-            holder.tv_title.setText(thread.getTitle());
+            holder.tv_thread_type.setVisibility(View.GONE);
         }
 
         holder.tv_title.setTextSize(HiSettingsHelper.getInstance().getTitleTextSize());
 
         holder.tv_replycounter.setText(thread.getCountCmts() + "/" + thread.getCountViews());
 
-//        if (thread.getCountViews() != null) {
-//            holder.tv_viewcounter.setText(thread.getCountViews());
-//        }
         holder.tv_create_time.setText(Utils.shortyTime(thread.getTimeCreate()));
-//        holder.tv_update_time.setText(Utils.shortyTime(thread.getTimeUpdate()));
 
         if (thread.getHavePic()) {
             holder.iv_image_indicator.setVisibility(View.VISIBLE);
@@ -88,10 +84,9 @@ public class ThreadListAdapter extends HiAdapter<ThreadBean> {
         ImageView avatar;
         TextView tv_title;
         TextView tv_author;
-        //        TextView tv_viewcounter;
+        TextView tv_thread_type;
         TextView tv_replycounter;
         TextView tv_create_time;
-        //        TextView tv_update_time;
         TextView iv_image_indicator;
     }
 }
