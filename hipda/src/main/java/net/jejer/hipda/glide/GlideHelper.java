60ac11d9e7d19145eecd4613bd98201a42c0c755
GreenSkinMonster <chartreuse.orc@gmail.com>
2015-12-25 16:31:50 +0800

    better avatar cache

--- hipda/src/main/java/net/jejer/hipda/glide/GlideHelper.java
+++ hipda/src/main/java/net/jejer/hipda/glide/GlideHelper.java
@@ -3,7 +3,10 @@ package net.jejer.hipda.glide;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.widget.ImageView;
@@ -32,9 +35,12 @@ import net.jejer.hipda.utils.Constants;
 import net.jejer.hipda.utils.HiUtils;
 import net.jejer.hipda.utils.Logger;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.util.concurrent.TimeUnit;
 
 import de.greenrobot.event.EventBus;
@@ -48,6 +54,7 @@ public class GlideHelper {
 
     private static LRUCache<String, String> NOT_FOUND_AVATARS = new LRUCache<>(512);
     private static File AVATAR_CACHE_DIR;
+    public static File DEFAULT_AVATAR_FILE;
 
     private static Drawable DEFAULT_USER_ICON;
 
@@ -96,6 +103,18 @@ public class GlideHelper {
             Glide.get(context).register(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(client));
 
             AVATAR_CACHE_DIR = Glide.getPhotoCacheDir(context, "avatar");
+
+            DEFAULT_USER_ICON = new IconicsDrawable(HiApplication.getAppContext(), GoogleMaterial.Icon.gmd_account_box).color(Color.LTGRAY).sizeDp(64);
+            DEFAULT_AVATAR_FILE = new File(AVATAR_CACHE_DIR, "default.png");
+            if (!DEFAULT_AVATAR_FILE.exists()) {
+                try {
+                    Bitmap b = drawableToBitmap(DEFAULT_USER_ICON);
+                    b.compress(Bitmap.CompressFormat.PNG, 100, new FileOutputStream(DEFAULT_AVATAR_FILE));
+                    b.recycle();
+                } catch (Exception e) {
+                    Logger.e(e);
+                }
+            }
         }
     }
 
@@ -104,20 +123,17 @@ public class GlideHelper {
     }
 
     public static void loadAvatar(BaseFragment fragment, ImageView view, String avatarUrl) {
-        if (DEFAULT_USER_ICON == null)
-            DEFAULT_USER_ICON = new IconicsDrawable(HiApplication.getAppContext(), GoogleMaterial.Icon.gmd_account_box).color(Color.LTGRAY);
-
-        if (NOT_FOUND_AVATARS.containsKey(avatarUrl)) {
-            view.setImageDrawable(DEFAULT_USER_ICON);
-        } else {
-            if (isOkToLoad(fragment))
-                Glide.with(fragment)
-                        .load(avatarUrl)
-                        .diskCacheStrategy(DiskCacheStrategy.NONE)
-                        .centerCrop()
-                        .error(DEFAULT_USER_ICON)
-                        .crossFade()
-                        .into(view);
+        if (isOkToLoad(fragment)) {
+            if (NOT_FOUND_AVATARS.containsKey(avatarUrl)) {
+                avatarUrl = DEFAULT_AVATAR_FILE.getAbsolutePath();
+            }
+            Glide.with(fragment)
+                    .load(avatarUrl)
+                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                    .centerCrop()
+                    .error(DEFAULT_USER_ICON)
+                    .crossFade()
+                    .into(view);
         }
     }
 
@@ -205,6 +221,27 @@ public class GlideHelper {
 
     public static boolean isOkToLoad(Fragment fragment) {
         return fragment != null && fragment.getActivity() != null && !fragment.isDetached();
+    }
+
+
+    private static Bitmap drawableToBitmap(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
+
+    private static InputStream bitmapToInputStream(Bitmap bitmap) {
+        int size = bitmap.getHeight() * bitmap.getRowBytes();
+        ByteBuffer buffer = ByteBuffer.allocate(size);
+        bitmap.copyPixelsToBuffer(buffer);
+        return new ByteArrayInputStream(buffer.array());
     }
 
 }
--- hipda/src/main/java/net/jejer/hipda/glide/OkHttpStreamFetcher.java
+++ hipda/src/main/java/net/jejer/hipda/glide/OkHttpStreamFetcher.java
@@ -72,45 +72,45 @@ public class OkHttpStreamFetcher implements DataFetcher<InputStream> {
 
                 if (!response.isSuccessful()) {
                     if (response.code() == 404) {
-                        GlideHelper.markAvatarNotFound(stringUrl);
                         if (!f.createNewFile())
                             Logger.e("create file failed : " + f.getName());
-                    }
-                    throw new IOException("Request failed with code: " + response.code());
-                }
+                    } else
+                        throw new IOException("Request failed with code: " + response.code());
+                } else {
+                    InputStream is = response.body().byteStream();
+                    BufferedInputStream input = null;
+                    OutputStream output = null;
 
-                InputStream is = response.body().byteStream();
-
-                BufferedInputStream input = null;
-                OutputStream output = null;
-
-                try {
-                    input = new BufferedInputStream(is);
-                    output = new FileOutputStream(f);
-                    int count;
-                    byte[] data = new byte[1024];
-                    while ((count = input.read(data)) != -1) {
-                        output.write(data, 0, count);
-                    }
-                    output.flush();
-                } catch (Exception e) {
-                    if (f.exists())
-                        f.delete();
-                } finally {
                     try {
-                        if (input != null)
-                            input.close();
-                        if (output != null)
-                            output.close();
-                        if (is != null)
-                            is.close();
-                    } catch (Exception ignored) {
+                        input = new BufferedInputStream(is);
+                        output = new FileOutputStream(f);
+                        int count;
+                        byte[] data = new byte[1024];
+                        while ((count = input.read(data)) != -1) {
+                            output.write(data, 0, count);
+                        }
+                        output.flush();
+                    } catch (Exception e) {
+                        if (f.exists())
+                            f.delete();
+                    } finally {
+                        try {
+                            if (input != null) input.close();
+                            if (output != null) output.close();
+                            if (is != null) is.close();
+                        } catch (Exception ignored) {
+                        }
                     }
                 }
             }
-        } else if (f.length() == 0) {
-            GlideHelper.markAvatarNotFound(stringUrl);
+        }
+        if (!f.exists()) {
+            //no memory cahce, avatar will be re-download in next ImageView
             return null;
+        } else if (f.length() == 0) {
+            //with memory cahce, avatar not found, will be re-download after one day
+            GlideHelper.markAvatarNotFound(stringUrl);
+            return new FileInputStream(GlideHelper.DEFAULT_AVATAR_FILE);
         }
         return new FileInputStream(f);
     }
