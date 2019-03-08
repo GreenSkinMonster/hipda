ae701da181c30c233f74538017724330a8d6c64a
GreenSkinMonster <chartreuse.orc@gmail.com>
2019-03-08 13:00:06 +0800

    process file in request listener

--- hipda/src/main/java/net/jejer/hipda/job/GlideImageJob.java
+++ hipda/src/main/java/net/jejer/hipda/job/GlideImageJob.java
@@ -5,6 +5,10 @@ import android.media.ExifInterface;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.path.android.jobqueue.Params;
 
 import net.jejer.hipda.bean.HiSettingsHelper;
@@ -19,6 +23,8 @@ import org.greenrobot.eventbus.EventBus;
 
 import java.io.File;
 import java.util.concurrent.ExecutionException;
+
+import androidx.annotation.Nullable;
 
 
 /**
@@ -52,12 +58,52 @@ public class GlideImageJob extends BaseJob {
     public void onRun() throws Throwable {
         ImageInfo imageInfo = ImageContainer.getImageInfo(mUrl);
         try {
-            File cacheFile = mRequestManager
+            mRequestManager
                     .download(mUrl)
+                    .listener(new RequestListener<File>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<File> target, boolean isFirstResource) {
+                            if (mNetworkFetch) {
+                                Logger.e(e);
+                                imageInfo.setStatus(ImageInfo.FAIL);
+                                String message = "";
+                                if (HiSettingsHelper.getInstance().isErrorReportMode()) {
+                                    message = "url : " + mUrl + "\n\nmessage : " + e.getMessage();
+                                }
+                                EventBus.getDefault().post(new GlideImageEvent(mUrl, -1, ImageInfo.FAIL, message));
+                            }
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(File resource, Object model, Target<File> target, DataSource dataSource, boolean isFirstResource) {
+                            processFile(imageInfo, resource);
+                            return false;
+                        }
+                    })
                     .onlyRetrieveFromCache(!mNetworkFetch)
                     .submit()
                     .get();
+        } catch (Exception e) {
+            if (mNetworkFetch) {
+                Logger.e(e);
+                imageInfo.setStatus(ImageInfo.FAIL);
+                String message = "";
+                if (HiSettingsHelper.getInstance().isErrorReportMode()) {
+                    message = "url : " + mUrl + "\n\nmessage : ";
+                    if (e instanceof ExecutionException && e.getCause() != null) {
+                        message += e.getCause().getMessage();
+                    } else {
+                        message += e.getMessage();
+                    }
+                }
+                EventBus.getDefault().post(new GlideImageEvent(mUrl, -1, ImageInfo.FAIL, message));
+            }
+        }
+    }
 
+    private void processFile(ImageInfo imageInfo, File cacheFile) {
+        if (!imageInfo.isSuccess()) {
             BitmapFactory.Options options = new BitmapFactory.Options();
             options.inJustDecodeBounds = true;
 
@@ -95,24 +141,8 @@ public class GlideImageJob extends BaseJob {
             if (orientation > 0)
                 imageInfo.setOrientation(orientation);
             ImageContainer.markImageReady(mUrl, imageInfo);
-
-            EventBus.getDefault().post(new GlideImageEvent(mUrl, -1, ImageInfo.SUCCESS));
-        } catch (Exception e) {
-            if (mNetworkFetch) {
-                Logger.e(e);
-                imageInfo.setStatus(ImageInfo.FAIL);
-                String message = "";
-                if (HiSettingsHelper.getInstance().isErrorReportMode()) {
-                    message = "url : " + mUrl + "\n\nmessage : ";
-                    if (e instanceof ExecutionException && e.getCause() != null) {
-                        message += e.getCause().getMessage();
-                    } else {
-                        message += e.getMessage();
-                    }
-                }
-                EventBus.getDefault().post(new GlideImageEvent(mUrl, -1, ImageInfo.FAIL, message));
-            }
         }
+        EventBus.getDefault().post(new GlideImageEvent(mUrl, 100, ImageInfo.SUCCESS));
     }
 
     @Override
